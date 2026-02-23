022d762c5a7841de496c0ca8aad23705f26d77b7
DanySfe <danysfe@example.com>
2026-02-23 10:41:25 -0500

    Add basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,9 @@ const router = createBrowserRouter([
     path: "/profile",
     Component: ProfilePage,
   },
-]);
+], {
+  basename: "/Spatial-Art-prototype-",
+});
 
 export default function App() {
   return (
